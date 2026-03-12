3b0b67ed55f3d6878edb7843833b29ec8f7e6ccc
huutai97 <phamhuutaigts@gmail.com>
2026-03-13 00:58:55 +0900

    Them API question, sua APi exam

--- src/main/java/com/example/examprepbackend/service/impl/QuestionServiceImpl.java
+++ src/main/java/com/example/examprepbackend/service/impl/QuestionServiceImpl.java
@@ -1,10 +1,36 @@
 package com.example.examprepbackend.service.impl;
 
+import com.example.examprepbackend.dto.response.QuestionResponse;
+import com.example.examprepbackend.dto.response.UserSummaryResponse;
+import com.example.examprepbackend.entity.Question;
+import com.example.examprepbackend.repository.QuestionRepository;
 import com.example.examprepbackend.service.QuestionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class QuestionServiceImpl implements QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    //Map question -> questionResponse
+    private QuestionResponse convertToDto(Question question) {
+        QuestionResponse questionResponse = new QuestionResponse();
+
+        BeanUtils.copyProperties(question, questionResponse);
+
+        questionResponse.setCategory(question.getCategory().getName());
+        questionResponse.setDifficulty(question.getDifficultyLevel());
+        return questionResponse;
+    }
+
+
+    @Override
+    public Page<QuestionResponse> getAllQuestions(Pageable pageable) {
+        return questionRepository.findAll(pageable).map(this::convertToDto);
+    }
 }
