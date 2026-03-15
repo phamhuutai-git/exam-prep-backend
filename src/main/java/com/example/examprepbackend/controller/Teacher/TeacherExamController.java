ab1facad7b803ffe25c6028b1686872a2d844d31
huutai97 <phamhuutaigts@gmail.com>
2026-03-15 17:44:24 +0900

    Fix loi database

--- src/main/java/com/example/examprepbackend/controller/Teacher/TeacherExamController.java
+++ src/main/java/com/example/examprepbackend/controller/Teacher/TeacherExamController.java
@@ -24,7 +24,7 @@ public class TeacherExamController {
     private final ExamService examService;
 
     @GetMapping
-    public ResponseEntity<BaseResponse<Page<ExamResponse>>> getAllExams(ExamRequestParam examRequestParam, @PageableDefault(size = 10, sort = "createDate", direction = Sort.Direction.DESC) Pageable pageable) {
+    public ResponseEntity<BaseResponse<Page<ExamResponse>>> getAllExams(ExamRequestParam examRequestParam, @PageableDefault(size = 10, sort = "createDate") Pageable pageable) {
         return ResponseEntity.ok().body(new BaseResponse<>(examService.getAllExams(examRequestParam, pageable), "get all"));
     }
 
