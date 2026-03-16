34eb94e851c51e43b94341927cecc5e5098b426f
TruongNgo-05 <ngoquangtruongjk05@gmail.com>
2026-03-16 22:21:48 +0700

    done merge tai

--- src/main/java/com/example/examprepbackend/controller/users/UsersController.java
+++ src/main/java/com/example/examprepbackend/controller/users/UsersController.java
@@ -3,10 +3,12 @@ package com.example.examprepbackend.controller.users;
 import com.example.examprepbackend.common.BaseResponse;
 import com.example.examprepbackend.dto.request.users.ChangePasswordRequest;
 import com.example.examprepbackend.dto.request.users.UserProfileUpdateRequest;
+import com.example.examprepbackend.dto.response.users.UserInfoResponse;
 import com.example.examprepbackend.dto.response.users.UserSummaryResponse;
 import com.example.examprepbackend.service.UsersService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -31,13 +33,13 @@ public class UsersController {
     public ResponseEntity<BaseResponse<UserSummaryResponse>> updateProfile(Authentication authentication, @Valid @RequestBody UserProfileUpdateRequest profileUpdateRequest) {
         return ResponseEntity.ok().body(new BaseResponse<>(usersService.updateProfile(authentication, profileUpdateRequest), "Profile updated successfully"));
     }
-    @GetMapping("/me")
-    public ResponseEntity<BaseResponse<UserInfoResponse>> getUser(Authentication authentication) {
-        if (authentication == null || !authentication.isAuthenticated()) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new BaseResponse<>(null, "Please login to access this resource"));
-        }
-        UserInfoResponse userSummary = usersService.getCurrentUser(authentication);
-        return ResponseEntity.ok().body(new BaseResponse<>(userSummary, "Get current user successfully"));
-    }
+//    @GetMapping("/me")
+//    public ResponseEntity<BaseResponse<UserInfoResponse>> getUser(Authentication authentication) {
+//        if (authentication == null || !authentication.isAuthenticated()) {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new BaseResponse<>(null, "Please login to access this resource"));
+//        }
+//        UserInfoResponse userSummary = usersService.getCurrentUser(authentication);
+//        return ResponseEntity.ok().body(new BaseResponse<>(userSummary, "Get current user successfully"));
+//    }
 
 }
