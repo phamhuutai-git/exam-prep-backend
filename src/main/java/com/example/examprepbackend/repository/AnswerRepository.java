c4ee82163baa57cf4f18090869939c7f35162283
huutai97 <phamhuutaigts@gmail.com>
2026-04-03 22:57:16 +0900

    sua query delete trong answer repository

--- src/main/java/com/example/examprepbackend/repository/AnswerRepository.java
+++ src/main/java/com/example/examprepbackend/repository/AnswerRepository.java
@@ -1,6 +1,9 @@
 package com.example.examprepbackend.repository;
 import com.example.examprepbackend.entity.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,7 +12,9 @@ import java.util.List;
 public interface AnswerRepository extends JpaRepository<Answer, Integer> {
     List<Answer> findByQuestionIdIn(List<Integer> questionIds);
 
-    void deleteByQuestion_Id(Integer questionId);
+    @Modifying
+    @Query("delete from Answer a where a.question.id = :questionId")
+    void deleteByQuestion_Id(@Param("questionId") Integer questionId);
 
     List<Answer> findByQuestion_Id(Integer questionId);
 }
